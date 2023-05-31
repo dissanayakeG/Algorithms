d31731a0b7e369344c1937c79c61567cc0f9c6a4
Madusanka Dissanayake <mdissanayake994@gmail.com>
2023-05-31 16:01:30 +0530

    binary trees DFS

---
+++ BinaryTrees/BinaryTree.js
@@ -0,0 +1,50 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+const e = new Node('e')
+const f = new Node('f')
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//     a
+//    / \
+//   b   c
+//  / \   \
+// d   e   f
+
+console.log('DFS values', dfsValues(a))
+console.log('DFS values', dfsValuesR(a))
+
+function dfsValues(root) {
+    if (root === null) return []; //empty tree
+    let stack = [root]
+    let result = []
+    while (stack.length > 0) {
+        const current = stack.pop()
+        result.push(current.val)
+        if (current.right) stack.push(current.right)
+        if (current.left) stack.push(current.left)
+    }
+    return result;
+}
+
+function dfsValuesR(root) {
+    if (root === null) return [];
+    const left = dfsValuesR(root.left)
+    const right = dfsValuesR(root.right)
+    return [root, ...left, ...right];
+}
+
