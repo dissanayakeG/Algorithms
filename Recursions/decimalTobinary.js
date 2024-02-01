32393743bf235fb656793ec848351be53168a9bc
Madusanka Dissanayake <mdissanayake994@gmail.com>
2024-02-01 17:25:16 +0530

    recursions

--- Recursions/decimalTobinary.js
+++ Recursions/decimalTobinary.js
@@ -1,3 +1,4 @@
+//Method 1
 const decimal = 233;
 console.log('Binary of ' + decimal, '-->', toBinary(decimal));
 
@@ -13,3 +14,44 @@ function toBinary(decimal) {
 
   return binary;
 }
+
+//Method 2
+function toBinary2(number, result = []) {
+
+  if (number == 0) {
+    return result;
+  }
+
+  result.unshift(number % 2)
+
+  return toBinary2(Math.floor(number / 2), result)
+}
+
+console.log('toBinary- ', toBinary2(233));
+
+
+function backToDecimal(list, originalLength, index, lastElement, result = null) {
+  index--
+  if (index < 0) {
+    return result;
+  }
+
+  result = result + Math.pow(2, (originalLength - index - 1)) * lastElement
+
+  return backToDecimal(list.slice(0, -1), originalLength, index, list.splice(-1, 1)[0], result);
+}
+
+let inputArray = [1, 1, 1, 0, 1, 0, 0, 1];
+
+console.log('backToDecimal-', backToDecimal(inputArray, inputArray.length, inputArray.length, inputArray.splice(-1, 1)[0]));
+
+// 1 -> 128
+// 1 -> 64
+// 1 -> 32
+// 0 -> 16
+// 1 -> 8
+// 0 -> 4
+// 0 -> 2
+// 1 -> 0 
+
+// console.log(1 + 8 + 32+ 64 + 128); //233
